d3ddb14ec73708587b150a03322032611cc0ab83
Axel Olsson <axel.n.olsson@outlook.com>
2026-04-23 10:10:29 +0200

    Simple implementation: no API implementation

---
+++ group7/lib/features/current_aqi/aqi_model.dart
@@ -0,0 +1,35 @@
+import "aqi_style.dart";
+
+
+// Homepage class, add more attributes to account for more data
+
+class AqiModel {
+  final int aqi;
+  final String city;
+  final DateTime timestamp;
+
+  AqiModel({required this.aqi, required this.city, required this.timestamp});
+
+
+  /* 
+  
+  This factory will be used to fetch data from thea AQI/MAP API. 
+  The structure of the json calls might need to be changed.
+  
+  */
+
+  factory AqiModel.fromJson(Map<String, dynamic> json) {
+    return AqiModel(
+      aqi: json["data"]["aqi"],
+      city: json["data"]["city"]["name"],
+      timestamp: DateTime.now(),
+    );
+  }
+
+
+  AqiStyle get style => AqiStyle.fromAqi(aqi);
+
+
+
+
+}
