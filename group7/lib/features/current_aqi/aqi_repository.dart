56d205218997f20fba7d18a8d15c10319ea1ecd3
Axel Olsson <axel.n.olsson@outlook.com>
2026-04-23 10:21:31 +0200

    Simple implementaion: No API

---
+++ group7/lib/features/current_aqi/aqi_repository.dart
@@ -0,0 +1,23 @@
+import 'aqi_model.dart';
+
+class AqiRepository {
+
+  /*
+
+  No HTTP or APi_keys yet! Implement later.
+
+  */
+
+  Future<AqiModel> fetchAqi(double lat, double lon) async {
+    await Future.delayed(const Duration(seconds: 2)); // Fake a wating time of 2s
+
+    // Return fake data¨
+    
+    return AqiModel(
+      aqi: 400, 
+      city: "", timestamp: 
+      DateTime.now()
+    );
+  }
+
+}
