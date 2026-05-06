da97119bb97bcb4e95a5278680386e519472fe8c
Hadi <dinemail.se>
2026-05-06 14:25:03 +0200

    h

--- group7/lib/features/current_aqi/current_aqi_screen.dart
+++ group7/lib/features/current_aqi/current_aqi_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:group7/core/favorites_screen.dart';
 import 'package:group7/core/widgets/pollutantText.dart';
 import 'package:provider/provider.dart';
 import 'aqi_provider.dart';
@@ -10,17 +11,16 @@ Very basic UI Screen for current AQI, needs to be improved
 
 */
 
-
 class CurrentAqiScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     final aqiProvider = context.watch<AqiProvider>();
 
     if (aqiProvider.isLoading) {
       return Scaffold(
         appBar: Navbar(),
-        body: Center(child: CircularProgressIndicator()));
+        body: Center(child: CircularProgressIndicator()),
+      );
     }
 
     final data = aqiProvider.currentData;
@@ -28,9 +28,25 @@ class CurrentAqiScreen extends StatelessWidget {
       return Scaffold(
         appBar: Navbar(),
         body: Center(
-          child: ElevatedButton(
-            onPressed: () => aqiProvider.refreshAqi(),
-            child: Text("Hämta luftkvalitet"),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton(
+                onPressed: () => aqiProvider.refreshAqi(),
+                child: Text("Hämta luftkvalitet"),
+              ),
+              SizedBox(height: 16),
+
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => FavoritesScreen()),
+                  );
+                },
+                child: Text("Gå till favoriter"),
+              ),
+            ],
           ),
         ),
       );
@@ -51,29 +67,49 @@ class CurrentAqiScreen extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              Text(data.city, style: TextStyle(color: Colors.white, fontSize: 32),textAlign: TextAlign.center),
+              Text(
+                data.city,
+                style: TextStyle(color: Colors.white, fontSize: 32),
+                textAlign: TextAlign.center,
+              ),
               Column(
                 children: [
-                  Text("${data.general}", style: TextStyle(color: Colors.white, fontSize: 100, fontWeight: FontWeight.bold)),
-                  Text(data.style.label, style: TextStyle(color: Colors.white, fontSize: 18)),
-                  Text(data.style.description, style: TextStyle(color: Colors.white, fontSize: 12), textAlign: TextAlign.center,),
+                  Text(
+                    "${data.general}",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 100,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    data.style.label,
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                  Text(
+                    data.style.description,
+                    style: TextStyle(color: Colors.white, fontSize: 12),
+                    textAlign: TextAlign.center,
+                  ),
                   SizedBox(height: 30),
                   PollutantText(value: data.pm2_5, subscript: "2.5"),
-                  SizedBox(height:8),
-                  PollutantText(value: data.pm10, subscript: "10" ),
+                  SizedBox(height: 8),
+                  PollutantText(value: data.pm10, subscript: "10"),
                   SizedBox(height: 40),
-                  Text(data.threeDayForecast[0].date, style: TextStyle(color: Colors.white, fontSize: 24)),
+                  Text(
+                    data.threeDayForecast[0].date,
+                    style: TextStyle(color: Colors.white, fontSize: 24),
+                  ),
                   SizedBox(
-                    width:300,
-                    child: Divider(
-                      color: Colors.white,
-                      thickness: 2,
-                    )
+                    width: 300,
+                    child: Divider(color: Colors.white, thickness: 2),
                   ),
                   SizedBox(height: 12),
                   SingleChildScrollView(
                     scrollDirection: Axis.horizontal,
-                    padding: const EdgeInsets.symmetric(horizontal: 16), // Gives space at start/end of scroll
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                    ), // Gives space at start/end of scroll
                     child: Row(
                       children: [
                         _buildForecastDay(data.threeDayForecast[1]),
@@ -84,11 +120,11 @@ class CurrentAqiScreen extends StatelessWidget {
                   ),
                 ],
               ),
-              
+
               IconButton(
                 icon: Icon(Icons.refresh, color: Colors.white, size: 40),
                 onPressed: () => aqiProvider.refreshAqi(),
-              )
+              ),
             ],
           ),
         ),
@@ -96,13 +132,21 @@ class CurrentAqiScreen extends StatelessWidget {
     );
   }
 }
+
 Widget _buildForecastDay(dynamic forecast) {
   return Padding(
     padding: const EdgeInsets.symmetric(horizontal: 12.0),
     child: Column(
       spacing: 2,
       children: [
-        Text(forecast.date, style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold)),
+        Text(
+          forecast.date,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
         _buildMetricRow("Avg", forecast.pm2_5.avg, forecast.pm10.avg),
         _buildMetricRow("Max", forecast.pm2_5.max, forecast.pm10.max),
         _buildMetricRow("Min", forecast.pm2_5.min, forecast.pm10.min),
