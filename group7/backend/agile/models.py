17fcd66515d1720ecb78c352dc169691b7b31457
Alexander Gustafsson <alexandergustafsson@MacBook-Pro-som-tillhor-Alexander.local>
2026-05-07 14:49:41 +0200

    API-calls are done and should work if you have setup the database

--- group7/backend/agile/models.py
+++ group7/backend/agile/models.py
@@ -2,11 +2,12 @@ from django.db import models
 
 class Users(models.Model):
     id = models.AutoField(primary_key=True)
-    username = models.CharField(max_length=30)
+    username = models.CharField(unique=True, max_length=10, null=True) # Set to null temp
 
 
 class UserFavourites(models.Model):
     id = models.AutoField(primary_key=True)
     userid = models.ForeignKey(Users, on_delete=models.CASCADE)
-    
+    lat = models.CharField(max_length=10)
+    lon = models.CharField(max_length=10)
 
--- group7/backend/agile/urls.py
+++ group7/backend/agile/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from .views import test_api
+from .views import create_user
+from .views import create_favourite
+from .views import fetch_users
+from .views import fetch_favourites
 
 urlpatterns = [
     path("api/test/", test_api),
+    path("api/createuser", create_user),
+    path("api/createfavourite", create_favourite),
+    path("api/fetchusers", fetch_users),
+    path("api/fetchfavourites", fetch_favourites)
 ]
